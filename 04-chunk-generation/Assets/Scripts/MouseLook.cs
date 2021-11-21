ceffe449bae92f317db231ede1696b4747120fb2
Lukas <games.luki@gmail.com>
2021-11-21 13:47:15 +0100

    Some cleanup

--- 04-chunk-generation/Assets/Scripts/MouseLook.cs
+++ 04-chunk-generation/Assets/Scripts/MouseLook.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MouseLook : MonoBehaviour
-{
-    public enum RotationAxis
-    {
+public class MouseLook : MonoBehaviour {
+    public enum RotationAxis {
         MouseXY = 0,
         MouseX = 1,
         MouseY = 2
@@ -19,21 +17,17 @@ public class MouseLook : MonoBehaviour
     private float _rotX = 0.0f;
 
     // Update is called once per frame
-    void Update()
-    {
-        if(axis == RotationAxis.MouseX)
-        {
+    void Update() {
+        if (axis == RotationAxis.MouseX) {
             transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
         }
-        else if(axis == RotationAxis.MouseY)
-        {
+        else if (axis == RotationAxis.MouseY) {
             _rotX -= Input.GetAxis("Mouse Y") * sensitivityVer;
             _rotX = Mathf.Clamp(_rotX, minVert, maxVert);
             float rotY = transform.localEulerAngles.y;
             transform.localEulerAngles = new Vector3(_rotX, rotY, 0);
         }
-        else
-        {
+        else {
             _rotX -= Input.GetAxis("Mouse Y") * sensitivityVer;
             _rotX = Mathf.Clamp(_rotX, minVert, maxVert);
             float delta = Input.GetAxis("Mouse X") * sensitivityHor;
