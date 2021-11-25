5f86e0e3f79d4675a0b4e75f8dec217e3883903c
Lukas <games.luki@gmail.com>
2021-11-25 21:44:49 +0100

    Fixed movement scripts and friction bugs

--- Project/Assets/Scripts/World.cs
+++ Project/Assets/Scripts/World.cs
@@ -6,18 +6,25 @@ public class World : MonoBehaviour
 {
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject myPrefab;
-
+    public PhysicMaterial worldMaterial = new PhysicMaterial {
+        staticFriction = 0f,
+        dynamicFriction = 0f,
+        bounciness = 0f,
+        frictionCombine = PhysicMaterialCombine.Minimum,
+        bounceCombine = PhysicMaterialCombine.Minimum
+    };
+    
     // This script will simply instantiate the Prefab when the game starts.
-    void Start()
-    {
+    void Start() {
+
         // Instantiate at position (0, 0, 0) and zero rotation.
         for(int x = 0; x < 3; x++)
         {
             for (int y = 0; y < 3; y++)
             {
                 Debug.Log("instantiate now");
-                Instantiate(myPrefab, new Vector3(17 * x, 1, 17 * y), Quaternion.identity).AddComponent<MeshCollider>(); //  This quaternion corresponds to "no rotation" - the object is perfectly aligned with the world or parent axes.
-
+                MeshCollider mc = Instantiate(myPrefab, new Vector3(17 * x, 1, 17 * y), Quaternion.identity).AddComponent<MeshCollider>(); //  This quaternion corresponds to "no rotation" - the object is perfectly aligned with the world or parent axes.
+                mc.material = worldMaterial;
             }
 
         }
