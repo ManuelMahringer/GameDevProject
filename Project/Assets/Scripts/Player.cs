f040dbbe8775a5ab0e5da62c8c99b5e1c057ef61
Lukas <games.luki@gmail.com>
2021-12-05 17:40:41 +0100

    Implemented fall damage and refactored Player.cs

--- Project/Assets/Scripts/Player.cs
+++ Project/Assets/Scripts/Player.cs
@@ -3,32 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.UIElements;
 
 
-// Credit: https://www.youtube.com/watch?v=NEUzB5vPYrE
+// Player Controller Basics: https://www.youtube.com/watch?v=NEUzB5vPYrE
+// Fall Damage: https://www.youtube.com/watch?v=D897sarRL3w
 public class Player : MonoBehaviour {
-    [Header("Walk / Run Settings")] 
+    [Header("Health")]
+    public float health = 100f;
+    public float fallDmgDistThreshold = 2f;
+    public float fallDmgMultiplier = 20f;
+
+    [Header("Walk / Run Settings")]
     public float walkSpeed = 3f;
     public float runSpeed = 5f;
 
-    [Header("Jump Settings")] 
+    [Header("Jump Settings")]
     public float jumpForce = 10_000f;
     public ForceMode appliedForceMode = ForceMode.Force;
 
-    [Header("Build/Destroy Settings")] 
+    [Header("Build/Destroy Settings")]
     public float hitRange = 5f;
 
-    [Header("Ground Tag Specification")] 
+    [Header("Ground Tag Specification")]
     public String groundTag = "";
     public GameObject highlightBlock;
 
-    [Header("Jumping State")] [SerializeField]
+    [Header("Jumping State")]
+    [SerializeField]
     private bool jump;
 
-    [SerializeField] private bool isGrounded;
+    [SerializeField]
+    private bool isGrounded;
 
-    [Header("Current Player Speed")] [SerializeField]
+    [Header("Current Player Speed")]
+    [SerializeField]
     private float currentSpeed;
+    private bool IsFalling => !isGrounded && _rb.velocity.y < 0;
 
     private GameObject _world;
     private Rigidbody _rb;
@@ -39,6 +50,21 @@ public class Player : MonoBehaviour {
     private Vector3 _dxz;
     private Vector3 _groundLocation;
     private bool _respawn;
+    private bool _wasGrounded;
+    private bool _wasFalling;
+    private float _startOfFall;
+
+    public void TakeDamage(float amount) {
+        Debug.Log("Take Damage: " + amount);
+        health -= amount;
+        if (health < 0) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Debug.Log("PLAYER DIES");
+    }
 
     private void Start() {
         _world = GameObject.Find("World");
@@ -52,18 +78,43 @@ public class Player : MonoBehaviour {
         jump = Input.GetButton("Jump");
         _respawn = Input.GetKey(KeyCode.R);
 
-        if (isGrounded) {
-            currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
-        }
+        bool run = Input.GetKey(KeyCode.LeftShift);
+        bool destroyBlock = Input.GetMouseButtonDown(0);
+        bool buildBlock = Input.GetMouseButtonDown(1);
+        bool shoot = Input.GetKey(KeyCode.T);
 
-        SetGrounded();
-        DestroyBlock();
-        BuildBlock();
-        Shoot();
+        if (isGrounded)
+            currentSpeed = run ? runSpeed : walkSpeed;
+        if (destroyBlock)
+            DestroyBlock();
+        if (buildBlock)
+            BuildBlock();
+        if (shoot)
+            Shoot();
+
         SetHighlightBlock();
     }
 
     private void FixedUpdate() {
+        CheckAndToggleGrounded();
+        
+        // Fall Damage
+        if (!_wasFalling && IsFalling) {
+            _startOfFall = transform.position.y;
+        }
+
+        if (!_wasGrounded && isGrounded) {
+            float fallDistance = _startOfFall - transform.position.y;
+            if (fallDistance > fallDmgDistThreshold) {
+                float fallDamage = (fallDistance - fallDmgDistThreshold) * fallDmgMultiplier;
+                TakeDamage(fallDamage);
+            }
+            Debug.Log("Fall Distance: " + (_startOfFall - transform.position.y));
+        }
+        
+        _wasGrounded = isGrounded;
+        _wasFalling = IsFalling;
+        
         // Move
         if (isGrounded) {
             _dxz = transform.TransformDirection(_xAxis, 0f, _zAxis);
@@ -86,7 +137,7 @@ public class Player : MonoBehaviour {
         }
     }
 
-    private void SetGrounded() {
+    private void CheckAndToggleGrounded() {
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out _hit, Mathf.Infinity)) {
             if (string.IsNullOrEmpty(groundTag) || String.Compare(_hit.collider.tag, groundTag, StringComparison.Ordinal) == 0)
                 _groundLocation = _hit.point;
@@ -100,33 +151,88 @@ public class Player : MonoBehaviour {
     }
 
     private void DestroyBlock() {
-        if (Input.GetMouseButtonDown(0)) {
-            // Pos middle of screen
-            Vector3 midPoint = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
-            //ray passing mittlde of screen
-            Ray ray = _camera.ScreenPointToRay(midPoint);
-            RaycastHit hit;
-            Debug.Log("SHOOOOOOOOOOOOOOT");
+        // Pos middle of screen
+        Vector3 midPoint = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
+        //ray passing mittlde of screen
+        Ray ray = _camera.ScreenPointToRay(midPoint);
+        RaycastHit hit;
+        Debug.Log("SHOOOOOOOOOOOOOOT");
 
-            if (Physics.Raycast(ray, out hit, hitRange)) {
-                if (hit.transform.gameObject.GetComponent<Chunk>() != null) // Check if its a chunk
-                {
-                    Debug.Log("Hit: " + hit.point);
-                    Debug.Log("Ray " + ray);
-                    GameObject chunk = hit.transform.gameObject;
-                    // try to shift hit.point a little bit to counter the index out of bounds error
+        if (Physics.Raycast(ray, out hit, hitRange)) {
+            if (hit.transform.gameObject.GetComponent<Chunk>() != null) // Check if its a chunk
+            {
+                Debug.Log("Hit: " + hit.point);
+                Debug.Log("Ray " + ray);
+                GameObject chunk = hit.transform.gameObject;
+                // try to shift hit.point a little bit to counter the index out of bounds error
 
-                    Vector3 localCoordinate = hit.point + (ray.direction / 10000.0f) - chunk.transform.position;
-                    chunk.GetComponent<Chunk>().DestroyBlock(localCoordinate);
-                    Destroy(chunk.GetComponent<MeshCollider>());
-                    MeshCollider mc = chunk.AddComponent<MeshCollider>();
-                    mc.material = _world.GetComponent<World>().worldMaterial;
+                Vector3 localCoordinate = hit.point + (ray.direction / 10000.0f) - chunk.transform.position;
+                chunk.GetComponent<Chunk>().DestroyBlock(localCoordinate);
+                Destroy(chunk.GetComponent<MeshCollider>());
+                MeshCollider mc = chunk.AddComponent<MeshCollider>();
+                mc.material = _world.GetComponent<World>().worldMaterial;
 
-                    //StartCoroutine(HitIndicator(hit.point));
-                    //_world.GetComponent<World>().DestroyBlock(hit.point);
-                }
+                //StartCoroutine(HitIndicator(hit.point));
+                //_world.GetComponent<World>().DestroyBlock(hit.point);
             }
         }
+    }
+
+    private void BuildBlock() {
+        // Pos middle of screen
+        Vector3 midPoint = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
+        //ray passing mittlde of screen
+        Ray ray = _camera.ScreenPointToRay(midPoint);
+        RaycastHit hit;
+        Debug.Log("SHOOOOOOOOOOOOOOT");
+        if (Physics.Raycast(ray, out hit, hitRange)) {
+            if (hit.transform.gameObject.GetComponent<Chunk>() != null) // Check if its a chunk
+            {
+                Debug.Log("Hit: " + hit.point);
+                Debug.Log("Ray " + ray);
+                //GameObject chunk = hit.transform.gameObject;
+                // try to shift hit.point a little bit to counter the index out of bounds error
+
+
+                GameObject chunk = GameObject.Find("World").GetComponent<World>().FindChunk(hit.point - (ray.direction / 10000.0f));
+                Vector3 localCoordinate = hit.point - (ray.direction / 10000.0f) - chunk.transform.position;
+                Debug.Log("localCoordinate" + localCoordinate);
+                chunk.GetComponent<Chunk>().BuildBlock(localCoordinate);
+                Destroy(chunk.GetComponent<MeshCollider>());
+                MeshCollider mc = chunk.AddComponent<MeshCollider>();
+                mc.material = _world.GetComponent<World>().worldMaterial;
+
+                //StartCoroutine(HitIndicator(hit.point));
+                //_world.GetComponent<World>().DestroyBlock(hit.point);
+            }
+        }
+    }
+
+    private void Shoot() {
+        // Pos middle of screen
+        Vector3 midPoint = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
+        //ray passing mittlde of screen
+        Ray ray = _camera.ScreenPointToRay(midPoint);
+        RaycastHit hit;
+        Debug.Log("SHOOOOOOOOOOOOOOT");
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.transform.gameObject.GetComponent<Chunk>() != null) // Check if its a chunk
+            {
+                Debug.Log("Hit: " + hit.point);
+                Debug.Log("Ray " + ray);
+
+                GameObject chunk = GameObject.Find("World").GetComponent<World>().FindChunk(hit.point - (ray.direction / 10000.0f));
+                Vector3 localCoordinate = hit.point + (ray.direction / 10000.0f) - chunk.transform.position;
+                Debug.Log("localCoordinate" + localCoordinate);
+                chunk.GetComponent<Chunk>().DamageBlock(localCoordinate, 100);
+                Destroy(chunk.GetComponent<MeshCollider>());
+                MeshCollider mc = chunk.AddComponent<MeshCollider>();
+                mc.material = _world.GetComponent<World>().worldMaterial;
+            }
+        }
+
+        // TODO: remove - Debug: player damages himself when shooting
+        TakeDamage(25f);
     }
 
     private void SetHighlightBlock() {
@@ -139,64 +245,6 @@ public class Player : MonoBehaviour {
         }
         else {
             highlightBlock.SetActive(false);
-        }
-    }
-
-    private void BuildBlock() {
-        if (Input.GetMouseButtonDown(1)) {
-            // Pos middle of screen
-            Vector3 midPoint = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
-            //ray passing mittlde of screen
-            Ray ray = _camera.ScreenPointToRay(midPoint);
-            RaycastHit hit;
-            Debug.Log("SHOOOOOOOOOOOOOOT");
-            if (Physics.Raycast(ray, out hit, hitRange)) {
-                if (hit.transform.gameObject.GetComponent<Chunk>() != null) // Check if its a chunk
-                {
-                    Debug.Log("Hit: " + hit.point);
-                    Debug.Log("Ray " + ray);
-                    //GameObject chunk = hit.transform.gameObject;
-                    // try to shift hit.point a little bit to counter the index out of bounds error
-
-
-                    GameObject chunk = GameObject.Find("World").GetComponent<World>().FindChunk(hit.point - (ray.direction / 10000.0f));
-                    Vector3 localCoordinate = hit.point - (ray.direction / 10000.0f) - chunk.transform.position;
-                    Debug.Log("localCoordinate" + localCoordinate);
-                    chunk.GetComponent<Chunk>().BuildBlock(localCoordinate);
-                    Destroy(chunk.GetComponent<MeshCollider>());
-                    MeshCollider mc = chunk.AddComponent<MeshCollider>();
-                    mc.material = _world.GetComponent<World>().worldMaterial;
-
-                    //StartCoroutine(HitIndicator(hit.point));
-                    //_world.GetComponent<World>().DestroyBlock(hit.point);
-                }
-            }
-        }
-    }
-
-    private void Shoot() {
-        if (Input.GetKeyDown(KeyCode.T)) {
-            // Pos middle of screen
-            Vector3 midPoint = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2);
-            //ray passing mittlde of screen
-            Ray ray = _camera.ScreenPointToRay(midPoint);
-            RaycastHit hit;
-            Debug.Log("SHOOOOOOOOOOOOOOT");
-            if (Physics.Raycast(ray, out hit)) {
-                if (hit.transform.gameObject.GetComponent<Chunk>() != null) // Check if its a chunk
-                {
-                    Debug.Log("Hit: " + hit.point);
-                    Debug.Log("Ray " + ray);
-
-                    GameObject chunk = GameObject.Find("World").GetComponent<World>().FindChunk(hit.point - (ray.direction / 10000.0f));
-                    Vector3 localCoordinate = hit.point + (ray.direction / 10000.0f) - chunk.transform.position;
-                    Debug.Log("localCoordinate" + localCoordinate);
-                    chunk.GetComponent<Chunk>().DamageBlock(localCoordinate, 100);
-                    Destroy(chunk.GetComponent<MeshCollider>());
-                    MeshCollider mc = chunk.AddComponent<MeshCollider>();
-                    mc.material = _world.GetComponent<World>().worldMaterial;
-                }
-            }
         }
     }
 }
